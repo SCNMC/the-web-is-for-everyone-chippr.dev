cf4658f5217013e2a74dca973fc1e487c14c6b3d
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-03-31 18:18:26 +0200

    added image folder with images

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const port = 3000
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 //serve public files 
-app.use(express.static('styles'))
+app.use(express.static('public'))
 
 // hook up template engine 
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
- app.get('/' , (req, res) => {
-    res.sendFile('index.ejs', {root: __views});
+app.get('/', (req, res) => {
+    // res.send('Hallo wereld!')
+    res.render('index', {
+      title: 'Dit is een index uit EJS',
+    })
 
-});
+})
  app.listen(process.env.PORT || 3000, () => console.log(`App avaialble on http://localhost:3000`))
 
 
